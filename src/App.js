91bcc8adadc16b23a36c39931ed534d691f8c025
LeDucLoi193 <dekaito193@gmail.com>
2021-11-23 22:10:18 +0700

    add suggest product page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import SignUp from './component/signup';
-import LogIn from './component/login';
-import Dashboard from './component/dashboard';
-import Home from './component/home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ForgotPassword from "./component/ForgotPassword";
+import React from 'react'
+import SignUp from './component/signup'
+import LogIn from './component/login'
+import Dashboard from './component/dashboard'
+import Home from './component/home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ForgotPassword from './component/ForgotPassword'
+import SuggestProduct from './component/suggestion'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<LogIn/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/forgot' element={<ForgotPassword/>}/>
-        <Route path='/' element={<LogIn/>}/>
-      </Routes>
+        <Routes>
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/forgot" element={<ForgotPassword />} />
+          <Route path="/suggest" element={<SuggestProduct />} />
+          <Route path="/" element={<LogIn />} />
+        </Routes>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
